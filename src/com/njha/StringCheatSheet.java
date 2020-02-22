26e1182cd0cf4f95ce0b750237007db80eaf42db
nkjha135 <nkjha2075@gmail.com>
2020-02-22 19:07:28 +0530

    adds cheat sheet for strings

---
+++ src/com/njha/StringCheatSheet.java
@@ -0,0 +1,28 @@
+package com.njha;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+
+public class StringCheatSheet {
+
+    public static void main(String[] args) {
+        /* BUILD A STRING FROM LIST OF INTEGERS */
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(1); l.add(2); l.add(3); l.add(4);
+        
+        StringJoiner joiner = new StringJoiner(", ");
+        for (Integer cs: l) {
+            joiner.add(cs.toString());
+        }
+        System.out.println(joiner.toString()); // 1, 2, 3, 4
+        
+        /* BUILD A STRING FROM LIST OF STRINGS */
+        List<String> sl = new ArrayList<String>();
+        sl.add("ab"); sl.add("cd"); sl.add("ef"); sl.add("gh");
+        String resultString = String.join(", ", sl);
+        System.out.println(resultString); // ab, cd, ef, gh
+
+    }
+    
+}
